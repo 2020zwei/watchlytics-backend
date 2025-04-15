1026acf6f3b629a639d27ea4c195b0b69b0a9adc
Syed Mudabbir Ali <mudabbir.tx@gmail.com>
2025-04-15 11:42:51 +0500

    subscription plan

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter()
-# router.register('plans', views.PlanViewSet)
-# router.register('subscriptions', views.SubscriptionViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('checkout/', views.CheckoutView.as_view(), name='checkout'),
-    # path('webhook/', views.StripeWebhookView.as_view(), name='webhook'),
-    # path('manage/', views.ManageSubscriptionView.as_view(), name='manage_subscription'),
+    path('plans/', views.PlanListAPIView.as_view(), name='plan-list'),
+    path('plans/<int:pk>/', views.PlanDetailAPIView.as_view(), name='plan-detail'),
+
+    path('subscription/', views.UserSubscriptionAPIView.as_view(), name='user-subscription'),
+    path('subscription/create/', views.CreateSubscriptionAPIView.as_view(), name='create-subscription'),
+    path('subscription/update/', views.UpdateSubscriptionAPIView.as_view(), name='update-subscription'),
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Plan, Subscription
+from .serializers import PlanSerializer, SubscriptionSerializer
 
-# Create your views here.
+class PlanListAPIView(generics.ListAPIView):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class PlanDetailAPIView(generics.RetrieveAPIView):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserSubscriptionAPIView(generics.RetrieveAPIView):
+    serializer_class = SubscriptionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.subscription
+
+
+class CreateSubscriptionAPIView(generics.CreateAPIView):
+    serializer_class = SubscriptionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class UpdateSubscriptionAPIView(generics.UpdateAPIView):
+    serializer_class = SubscriptionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.subscription
