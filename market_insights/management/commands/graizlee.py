82fdd90fdcd00f9e6f6bc2a8b53175a561356049
Syed Mudabbir Ali <mudabbir.tx@gmail.com>
2025-05-19 12:52:32 +0500

    update

--- market_insights/management/commands/graizlee.py
+++ market_insights/management/commands/graizlee.py
@@ -147,8 +147,8 @@ class Command(BaseCommand):
         options = webdriver.ChromeOptions()
         
         # Create a temporary unique user profile directory
-        unique_dir = os.path.join(tempfile.gettempdir(), f"chrome_profile_{uuid.uuid4().hex}")
-        options.add_argument(f"--user-data-dir={unique_dir}")
+        # unique_dir = os.path.join(tempfile.gettempdir(), f"chrome_profile_{uuid.uuid4().hex}")
+        # options.add_argument(f"--user-data-dir={unique_dir}")
         
         # Add anti-detection measures
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.91 Safari/537.36")
