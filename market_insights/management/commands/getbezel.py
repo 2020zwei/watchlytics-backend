a64ded95d72993e4bc3d04ffbdad906ac4b10f1f
Syed Mudabbir Ali <mudabbir.tx@gmail.com>
2025-05-19 14:40:25 +0500

    bezel data update

--- market_insights/management/commands/getbezel.py
+++ market_insights/management/commands/getbezel.py
@@ -423,7 +423,7 @@ class Command(BaseCommand):
                 
                 # Create new MarketData entry
                 market_data = MarketData(
-                    source='Bezel',
+                    source='bezel',
                     price=price_value,
                     item_id=item_id,
                     name=watch.get('model'),
